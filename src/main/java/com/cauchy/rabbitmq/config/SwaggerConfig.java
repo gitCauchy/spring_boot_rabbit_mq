63343f5f8436c21192eb6b37045d791356e57032
Cauchy <mailcauchy@gmail.com>
2023-02-09 16:33:04 +0800

    first commit

---
+++ src/main/java/com/cauchy/rabbitmq/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.cauchy.rabbitmq.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket webApiConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("WebApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .build();
+    }
+
+    private ApiInfo webApiInfo() {
+        return new ApiInfoBuilder()
+                .title("rabbitmq 文档")
+                .description("文档描述了 rabbit mq 微服务接口定义")
+                .version("1.0")
+                .contact(new Contact("cauchy", "http://cauchy.com", "mailcauchy@gmail.com"))
+                .build();
+    }
+}
